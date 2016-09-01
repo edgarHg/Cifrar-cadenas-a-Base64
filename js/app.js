7107869bcccf7f55907825c8b28433b0caf80089
EdgarHg <iscehg@gmail.com>
2016-08-31 23:07:21 -0500

    Cambio de variables de $scope por objeto de $scope

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
 angular.module('App', ['fService'])
 	.controller('ctrlCifrar', ['$scope', 'CryptoJSAES', function($scope, CryptoJSAES){
+		$scope.objIteracion = {
+			mdlCadena: "",
+			cadenaCifrada: "",
+			cadenaDescifrada: "",
+			keyUsuario: 30082016
+		};
 
-		$scope.mdlCadena = "";
-		$scope.cadenaCifrada = "";
-		$scope.cadenaDescifrada = "";
-		$scope.keyUsuario = 30082016;
 		$scope.CifrarCadena = function(){
-			$scope.cadenaCifrada = CryptoJSAES.cifrar($scope.mdlCadena, $scope.keyUsuario);
+			$scope.objIteracion.cadenaCifrada = CryptoJSAES.cifrar($scope.objIteracion.mdlCadena, $scope.objIteracion.keyUsuario);
+
 		};
 
 		$scope.DescifrarBase64 = function(){
-			$scope.cadenaDescifrada = CryptoJSAES.descifrar($scope.cadenaCifrada, $scope.keyUsuario);
+			$scope.objIteracion.cadenaDescifrada = CryptoJSAES.descifrar($scope.objIteracion.cadenaCifrada, $scope.objIteracion.keyUsuario);
 		};
 	}])
